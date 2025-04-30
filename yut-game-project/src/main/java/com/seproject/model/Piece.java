6de4a03a17657c3e8c756cf00eb76bd83245cc8e
Seung-Un <un7476@naver.com>
2025-05-01 00:50:01 +0900

    implement Player, Piece, EachPiece

--- yut-game-project/src/main/java/com/seproject/model/Piece.java
+++ yut-game-project/src/main/java/com/seproject/model/Piece.java
@@ -1,14 +1,38 @@
 public class Piece {
-    public Piece(int playerId){}; // 생성자
+    public Piece(int pieceId, int playerId){
+        this.pieceId=pieceId;
+        this.playerId=playerId;
+        this.currentPathNodeId=0;
+        pieces = new ArrayList<>();
+        pieces.add(new EachPiece(pieceId, playerId)); //기본 eachPiece 객체 하나 배열에 저장
+    }; // 생성자
 	private int pieceId;
 	private int playerId;
     private int currentPathNodeId; //0으로 초기화
 
-	private EachPiece[] pieces; // 기본으로 배열에 EachPiece를 하나씩 갖고, 그룹화가 필요할 때마다 여기에 하나씩 추가된다. 이런 식으로 그룹화에 관한 사항을 내부적으로 구현할 수 있다.}
-    public void setPathNodeId(int pathNodeId){};
+    //동적 배열로 선언하였음
+	private ArrayList<EachPiece> pieces; // 기본으로 배열에 EachPiece를 하나씩 갖고, 그룹화가 필요할 때마다 여기에 하나씩 추가된다. 이런 식으로 그룹화에 관한 사항을 내부적으로 구현할 수 있다.}
+
+    public void setPathNodeId(int pathNodeId){
+        currentPathNodeId=pathNodeId;
+    };
     //이동시킨다. 게임 시작할 때 0으로 이동하고, 게임 하는 도중에도 계속 쓸 예정이다.
-    public int getPieceId;
-    public int getPlayerId;
-    public EachPiece[] getEachPieces(){}; // 이 그룹에 포함된 말들을 반환하는 getter
-    public separatePieceGroup(){};
+    public int getPieceId(){
+        return pieceId;
+    };
+    public int getPlayerId(){
+        return playerId;
+    };
+    public ArrayList<EachPiece> getEachPieces(){
+        return pieces;
+    }; // 이 그룹에 포함된 말들을 반환하는 getter
+    public void groupPiece(Piece group){
+        for(EachPiece piece : group.getEachPieces()){
+            pieces.add(piece);
+        }
+    }; //말을 업는 기능 Piece를 인자로 받아서 인자에 포함되어 있는 eachPiece를 전부 현재 객체의 pieces에 추가한다
+    //양쪽 Piece 모두 eachPiece를 추가해서 동일한 정보의 객체를 2개 유지할것인지 한쪽에만 추가하고 다른 한쪽은 제거할것인지 고민 필요
+    public separatePieceGroup(){
+
+    }; //piece를 분리하는 기능이 꼭 필요한가? 말이 잡혔을 경우 객체 자체를 제거하고 다시 출발할 때 piece 객체를 생성하면 어떨까?
 }
--- yut-game-project/src/main/java/com/seproject/model/Player.java
+++ yut-game-project/src/main/java/com/seproject/model/Player.java
@@ -1,6 +1,10 @@
+package com.seproject.model;
+
 public class Player {
-    public players(String nickname){
+    public Player(String nickname, int playerId){
         //닉네임을 이용해 플레이어 클래스를 생성한다. 팀 번호는 더 이상 의미가 없으므로 인자로 받지 않는다.
+        this.name=nickname; //입력 받은 이름 설정
+        this.playerId=playerId;
     };
 
     private String name; //인터페이스로 입력 받은 이름이다. 닉네임도 상관없다.
@@ -8,9 +12,11 @@ public class Player {
     
     public String getName(){
         //이름을 반환한다.
+        return name;
     };  
 
     public int getId(){
         //자신의 유저 아이디를 반환한다.
+        return playerId;
     }; 
 }
