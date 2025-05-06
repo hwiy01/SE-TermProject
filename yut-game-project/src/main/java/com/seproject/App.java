4dc73c843f00ef7a98ff5e8955a263986d46d790
ERIC <softcau@naver.com>
2025-05-06 23:21:30 +0900

    오타 수정

--- yut-game-project/src/main/java/com/seproject/App.java
+++ yut-game-project/src/main/java/com/seproject/App.java
@@ -14,7 +14,7 @@ public class App
         // GameManager 객체 생성
         // GameManager의 playGame 함수 호출, 모든 실행 여기서 관여
         javax.swing.SwingUtilities.invokeLater(() -> {
-            GameManager gameManager = new GameManager(); // 네가 만든 GameManager 객체 생성
+            GameManager gameManager = new GameManager();
             new GameSetupUI(gameManager); // UI 실행
         });
         // 1. 
@@ -22,10 +22,8 @@ public class App
         // GameSetupUI.ShowMainMenu() 호출 //  아래의 옵션들
         // GameSetupUI.ShowNumberOfPiecesInEachPlayerChoice(),  showNumberOfPlayersChoice(), showBoardShapeChoice() showNicknameInput() 호출하여 첫 게임에 필요한 환경설정
 
-
         // 2. 
-        // Board 객체 생성 - 이때 numberOfShpae 넘겨줌
+        // Board 객체 생성 - 이때 numberOfShape 넘겨줌
         // GameManager 객체 생성하여 Board와 위의 옵션들 넘겨줌
-        System.out.println( "Hello World!" );
     }
 }
